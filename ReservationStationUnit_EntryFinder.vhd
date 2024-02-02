4fae4a7046849c494cbbacbed2c5b48fe80dccb5
EnzEdu <enzinhovlogs@outlook.com>
2024-02-01 22:38:16 -0300

    fix: Tags dedicadas para cada instrucao/entry

--- ReservationStationUnit_EntryFinder.vhd
+++ ReservationStationUnit_EntryFinder.vhd
@@ -4,9 +4,9 @@ USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY ReservationStationUnit_EntryFinder IS
     PORT(
-        Busy_Vector         :  IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+        Busy_Vector         :  IN STD_LOGIC_VECTOR(3 DOWNTO 0); -- RSU_BUSY(3) & RSU_BUSY(2) & RSU_BUSY(1) & RSU_BUSY(0)
 
-        Inst_0_Entry        : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+        Inst_0_Entry        : OUT STD_LOGIC_VECTOR(2 DOWNTO 0); 
         Inst_1_Entry        : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
         Inst_2_Entry        : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
         Inst_3_Entry        : OUT STD_LOGIC_VECTOR(2 DOWNTO 0)
@@ -16,30 +16,21 @@ END ENTITY ReservationStationUnit_EntryFinder;
 ARCHITECTURE behavior OF ReservationStationUnit_EntryFinder IS
 BEGIN
 
-    -- Identifies the index of first available entry on the Reservation Station,
-    -- based on it's Busy bit vector
+    -- Correlates every upcoming instruction on the RSU with a position on the buffer, and returns the tag
+      -- If that position on the buffer is occupied, the tag is "100" (invalid)
+      -- Else, the real tag is returned
+    -- Based on the RSU_BUSY bit vector
+    
     Inst_0_Entry <= "000" WHEN (Busy_Vector(0) = '0') ELSE
-                    "001" WHEN (Busy_Vector(1) = '0') ELSE
-                    "010" WHEN (Busy_Vector(2) = '0') ELSE
-                    "011" WHEN (Busy_Vector(3) = '0') ELSE
                     "100";
 
     Inst_1_Entry <= "001" WHEN (Busy_Vector(1) = '0') ELSE
-                    "010" WHEN (Busy_Vector(2) = '0') ELSE
-                    "011" WHEN (Busy_Vector(3) = '0') ELSE
-                    "000" WHEN (Busy_Vector(0) = '0') ELSE
                     "100";
 
     Inst_2_Entry <= "010" WHEN (Busy_Vector(2) = '0') ELSE
-                    "011" WHEN (Busy_Vector(3) = '0') ELSE
-                    "000" WHEN (Busy_Vector(0) = '0') ELSE
-                    "001" WHEN (Busy_Vector(1) = '0') ELSE
                     "100";
 
     Inst_3_Entry <= "011" WHEN (Busy_Vector(3) = '0') ELSE
-                    "000" WHEN (Busy_Vector(0) = '0') ELSE
-                    "001" WHEN (Busy_Vector(1) = '0') ELSE
-                    "010" WHEN (Busy_Vector(2) = '0') ELSE
                     "100";
 
 END ARCHITECTURE behavior;
